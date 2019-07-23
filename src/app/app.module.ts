66925d742c425cbfc2cf2754c6a22271eb5f1747
Pablo <phuerta@acsyt.com>
2019-07-22 21:09:32 -0500

    Added http module and created users service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { usersReducer} from './reducers/users.reducer';
+import { HttpClientModule,HttpErrorResponse } from '@angular/common/http';
+//services
+import {UsersService} from './services/users.service'
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    StoreModule.forRoot({user:usersReducer}),
+    HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    UsersService
   ],
   bootstrap: [AppComponent]
 })
